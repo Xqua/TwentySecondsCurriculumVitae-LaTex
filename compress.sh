f8afa62f5b341bdb5c3ee93d17f79f7806fd9c73
Leo Blondel <leo@blondel.ninja>
2021-06-25 17:33:27 +0200

    adding publication list

--- compress.sh
+++ compress.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 NAME=LeoBlondel_CV_$(LANG=en_us_88591; date "+%B")_$(date +%Y).pdf
 echo Compressing and saving to $NAME
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$NAME Twenty-Seconds_cv.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$NAME Twenty-Seconds_cv.pdf
+
+NAME=LeoBlondel_PublicationList_$(LANG=en_us_88591; date "+%B")_$(date +%Y).pdf
+echo Compressing and saving to $NAME
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$NAME PublicationList.pdf
+
+
